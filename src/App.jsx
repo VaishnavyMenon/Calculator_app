fefd21043f455fbc2cd2733f76a5f882d5d13de9
Vaishnavy <vaishnavymenon21@gmail.com>
2023-10-19 21:11:37 +0530

    lottie-animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import Calculator from './components/Calculator'
+import Container from './components/Container'
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500); // Set loading to false after 3 seconds
+  }, []);
+
   return (
-    <div className="body" >
-      <Calculator />
+    <div>
+      {loading? <Container/> : <Calculator/>}
     </div>
   )
 }
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,11 +1,28 @@
-import Calculator from './Calculator'
+// import React from "react";
+import { useLottie } from "lottie-react";
+import groovyWalkAnimation from "../assets/loading-animation.json";
 
 const Container = () => {
-  return (
-    <div style={{backgroundColor:"red"}}>
-    <Calculator/>
-    </div>
-  )
-}
+  const options = {
+    animationData: groovyWalkAnimation,
+    loop: false,
+  };
 
-export default Container
+  const { View } = useLottie(options);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        backgroundColor: "white",
+      }}
+    >
+      {View}
+    </div>
+  );
+};
+
+export default Container;
